1ccd318ef1fe8bef9f0c0c13e4a5f59eff496927
avrohomthousman <ahousman@student.touro.edu>
2023-02-10 14:07:12 -0500

    implemented all new methods other than checking for game over.

--- src/main/java/com/example/othello/model/OthelloGreedyAlgorithm.java
+++ src/main/java/com/example/othello/model/OthelloGreedyAlgorithm.java
@@ -30,23 +30,60 @@ public class OthelloGreedyAlgorithm implements OthelloModel {
     };
     protected LinkedList<Position> greenSpots;
     protected HashMap<Position, TileColor> nonGreenSpots;
+    private int blackPieces, whitePieces;
+    private boolean playersTurn;
+    private boolean gameIsRunning = false;
+
+
 
     public OthelloGreedyAlgorithm(){
         this.greenSpots = new LinkedList<>();
         this.nonGreenSpots = new HashMap<>();
     }
 
+
     @Override
     public HashMap<Position, TileColor> startGame() {
+        resetBoard();
+
+
+        gameIsRunning = true;
+        playersTurn = true;
+        blackPieces = 2;
+        whitePieces = 2;
+
+        return new HashMap<Position, TileColor>(nonGreenSpots);//deep copy
+    }
+
+
+    /**
+     * Clears the entire board and refills it with the starting pieces.
+     */
+    private void resetBoard(){
         nonGreenSpots.clear();
         greenSpots.clear();
 
+        setStartingNonGreenSpots();
+        setStartingGreenSpots();
+    }
+
+
+    /**
+     * Sets up the board with all the non-green spots that should be present at the start of the game.
+     */
+    private void setStartingNonGreenSpots(){
         //fill hashmap with starting values
         nonGreenSpots.put(new Position(3, 3), TileColor.WHITE);
         nonGreenSpots.put(new Position(4, 4), TileColor.WHITE);
         nonGreenSpots.put(new Position(4, 3), TileColor.BLACK);
         nonGreenSpots.put(new Position(3, 4), TileColor.BLACK);
+    }
 
+
+    /**
+     * Sets up the board with all the green spots that should be present at the start of the game.
+     */
+    private void setStartingGreenSpots(){
         //fill linkedList with starting values
         for (int i = 0; i < DIMENSIONS; i++){
             for(int j = 0; j < DIMENSIONS; j++){
@@ -54,34 +91,53 @@ public class OthelloGreedyAlgorithm implements OthelloModel {
             }
         }
         greenSpots.removeAll(nonGreenSpots.keySet());
-
-        return new HashMap<Position, TileColor>(nonGreenSpots);//deep copy
     }
+
 
     @Override
     public List<Position> getPlayerMove(Position playerMove) throws IllegalMoveException {
-        if(nonGreenSpots.containsKey(playerMove)){//if the player tried to go in a non-empty spot
+        if(!gameIsRunning){
+            throw new IllegalMoveException("Turns cannot be taken after the game ended.");
+        }
+        if(!playersTurn){
+            throw new IllegalMoveException("Its not your turn");
+        }
+        if(nonGreenSpots.containsKey(playerMove)){      //if the player tried to go in a non-empty spot
             throw new IllegalMoveException("players can only go in unoccupied spots");
         }
 
         List<Position> tilesFlipped = new ArrayList<>();
-        tilesFlipped.add(playerMove);//the actual place he went needs to become white
+        tilesFlipped.add(playerMove);    //the actual place he went needs to become white
         tilesFlipped.addAll(getCapturedSpots(playerMove, true));
 
         //make sure the chosen move is legal
         if(tilesFlipped.size() == 1){//the only spot that was changed was the spot he chose.
-            throw new IllegalMoveException("players can only go in spots that cause at least one peice to be captured.");
+            throw new IllegalMoveException("players can only go in spots that cause at least one piece to be captured.");
         }
+
         //update greenSpots and nonGreenSpots
         for(Position p : tilesFlipped){
             nonGreenSpots.put(p, TileColor.WHITE);
         }
         greenSpots.remove(playerMove);//that's the only one that started as green
+
+
+        whitePieces += tilesFlipped.size();
+        playersTurn = false;
+
         return tilesFlipped;
     }
 
+
     @Override
     public List<Position> getComputerMove() {
+        if(!gameIsRunning){
+            throw new IllegalStateException("Computer turn was requested when the game is already over.");
+        }
+        if(playersTurn){
+            throw new IllegalStateException("Computer turn was requested when it was the players turn to go.");
+        }
+
         //find the position that captures the most spots
         int maxCaptured = 0;
         Position maxPosition = null;
@@ -111,18 +167,25 @@ public class OthelloGreedyAlgorithm implements OthelloModel {
             nonGreenSpots.put(p, TileColor.BLACK);
         }
         greenSpots.remove(maxPosition);
+
+
+        playersTurn = true;
+        blackPieces += tilesFlipped.size();
+
+        //TODO: check if game ends becuase there are no more moves for the player
+
         return tilesFlipped;
     }
 
 
     /**
      * Calculates how many spaces change color as a result of the specified move, not
-     * including the move itself. The NUMBER of spots captured is calculated, but the actual
+     * including the move itself. The number of spots captured is calculated, but the actual
      * spots are not specified.
      *
-     * This method looks at the changes that happen in all 8
-     * directions away from the chosen spot. South, South-West, West, North-West, North,
-     * North-East, East, and South-East. These 8 directions are found in the static array 'shifts'
+     * This method looks at the changes that happen in all 8 directions away from the chosen spot.
+     * South, South-West, West, North-West, North, North-East, East, and South-East. These 8
+     * directions are found in the static array 'shifts'
      *
      * This method is used to help the computer decide which move to take.
      *
@@ -161,7 +224,7 @@ public class OthelloGreedyAlgorithm implements OthelloModel {
             if(color == null){//if that spot is not in the hashmap because its green
                 return 0;
             }
-            else if(color == TileColor.BLACK){//if you hit your own piece
+            else if(color == TileColor.BLACK){//if you hit your own piece FIXME: only works for computer move
                 return peicesCaptured;
             }
             else{
@@ -170,6 +233,7 @@ public class OthelloGreedyAlgorithm implements OthelloModel {
         }
         return 0; //this happens when all the pieces are white till the end of the board
     }
+
 
     /**
      * Finds all the spots that change color as a result of a players move, including the spot
@@ -195,6 +259,7 @@ public class OthelloGreedyAlgorithm implements OthelloModel {
 
         return spots;
     }
+
 
     /**
      * Finds all the spots that change color as a result of the chosen move.
@@ -232,29 +297,64 @@ public class OthelloGreedyAlgorithm implements OthelloModel {
     }
 
 
-    @Override
-    public boolean isPlayersTurn() {
+    /**
+     * Checks if the specified player still has at least one legal move, and the game is
+     * not yet over.
+     *
+     * @param forPlayer the player we are checking.
+     * @return true if there is at least one legal move for the specified player (and thus the
+     *      game continues), or false otherwise.
+     */
+    private boolean thereAreLegalMovesLeft(boolean forPlayer){
+        for(Position p : greenSpots){
+            if(getCaptureCount(p) > 0){ //FIXME: this wont work because this method assumes thePlayer is the computer
+                return true;
+            }
+        }
+
         return false;
     }
+
+
+    @Override
+    public boolean isPlayersTurn() {
+        return this.playersTurn;
+    }
+
 
     @Override
     public boolean isGameOver() {
-        return false;
+        return !gameIsRunning;
     }
+
 
     @Override
     public TileColor getWinner() {
-        return null;
+        if(gameIsRunning){
+            return null;
+        }
+
+        if(whitePieces > blackPieces){
+            return TileColor.WHITE;
+        }
+        else if(blackPieces > whitePieces){
+            return TileColor.BLACK;
+        }
+        else{
+            return null;
+        }
     }
+
 
     @Override
     public int getScoreOfBlackPlayer() {
-        return 0;
+        return blackPieces;
     }
+
 
     @Override
     public int getScoreOfWhitePlayer() {
-        return 0;
+        return whitePieces;
     }
 
 
